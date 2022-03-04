1258795847289242b07c78fb015b0f873b348e97
alex1c <alex1c.spb@gmail.com>
2022-03-04 19:10:59 +0300

    main

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import {Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer} from '@chakra-ui/react'
 import {FcMenu, FcHome, FcAbout} from 'react-icons/fc'
 import {BsSearch} from 'react-icons/bs'
